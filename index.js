d13e82e7fb10ffadfc8448c9dc6c13a901abe148
Pratik1803 <pratikvaidya1803@gmail.com>
2022-04-29 18:19:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const kanjiRoute = require('./api/kanji');
+const userRoute = require("./api/user");
+const PORT = process.env.PORT || 8000;
+
+app.use(kanjiRoute);
+app.use(userRoute);
+
+app.get("/api", (req,res)=>{
+    res.json({message:"Hello from /"});
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Listening to port ${PORT}...`);
+});
